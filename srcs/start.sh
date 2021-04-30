39f59da146aaadbcd8cd2321e50179ea680f07a5
asorrent06200 <73552720+asorrent06200@users.noreply.github.com>
2021-04-30 18:20:35 +0200

    bla

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,9 +1,10 @@
 #CERTIFICAT SSL
 mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=emma/CN=localhost"
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paca/L=Nice/O=42 School/OU=asorrent/CN=localhost"
 #NGINX
 mkdir var/www/localhost
 mv ./default etc/nginx/sites-available
+cp /var/www/html/index.nginx-debian.html /var/www/localhost
 ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
 chown -R www-data /var/www/*
 chmod -R 755 /var/www/*
